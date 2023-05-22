fade21a6fb21cb43668b5e139af1763300197709
GITvoren <tejuco.voren@gmail.com>
2023-05-22 11:34:58 +0800

    ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import Navbar from './components/Navbar.js'
+import AccountPanel from './components/AccountPanel.js'
 import {Routes, Route} from 'react-router-dom'
+import './assets/css/app.css'
+import trash from './assets/images/trash.png'
+import edit from './assets/images/edit.png'
+import check from './assets/images/check.png'
+import plus from './assets/images/plus.png'
 
 
 function App(){
   return(
-    <div>
-      <Navbar />
-      <Routes>
-        
-      </Routes>  
+    <div> 
+         <AccountPanel />
+          <div className="app">
+            
+            <div className="flex">
+            <h1>Todo App</h1>
+              <div className="flex-row">
+                <input type="text" className="add-input" />
+                <img src={plus} className="icon" />
+              </div>
+              <br /><br />
+              <hr className="hr" />
+              <div className="task-list">
+                  <p>task descas asdas asd asadsa asd</p>
+                  <div className="flex-row">
+                    <img src={check} className="icon"/>
+                    <img src={edit} className="icon"/>
+                    <img src={trash} className="icon"/>
+                  </div>
+              </div>
+              <div className="task-list">
+                  <p>task descas asdas asd asadsa asd</p>
+                  <div className="flex-row">
+                    <img src={check} className="icon"/>
+                    <img src={edit} className="icon"/>
+                    <img src={trash} className="icon"/>
+                  </div>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
---
+++ src/components/AccountPanel.js
@@ -0,0 +1,68 @@
+import '../assets/css/accountpanel.css'
+import {useState, useRef} from 'react'
+
+
+function AccountPanel(){
+
+     const [toggled, setToggled] = useState(false)
+     const ref = useRef(null);
+     const ref2 = useRef(null);
+
+     const toggle = () => {
+          const logForm = ref.current
+          const regForm = ref2.current
+          
+          if(!toggled){
+               logForm.classList.toggle('hidden')
+               regForm.classList.toggle('show')
+          } else{
+               
+               regForm.classList.toggle('hidden')
+               logForm.classList.toggle('show')
+          }
+
+     }
+
+   
+
+     return(
+          <div className="acc-panel show">
+               <h1>To Do's</h1>
+               <form className="log-form" ref={ref}>
+                    <div>
+                         <label>Username</label><br />
+                         <input type="text" className="acc-input"/>
+                    </div>
+                    <div>
+                         <label>Password</label><br />
+                         <input type="password" className="acc-input" />
+                    </div>
+                    <button>Login</button>
+                    <div>
+                         <span>No account yet?</span> <span onClick ={toggle} className="log-link">Register</span>
+                    </div>
+               </form>
+
+               <form className="reg-form hidden" ref={ref2}>
+                    <div>
+                         <label>Username</label><br />
+                         <input type="text" className="acc-input" />
+                    </div>
+                    <div>
+                         <label>Password</label><br />
+                         <input type="password" className="acc-input" />
+                    </div>
+                    <div>
+                         <label>Confirm Password</label><br />
+                         <input type="password" className="acc-input" />
+                    </div>
+                    <button>Register</button>
+                    <div>
+                         <span>Already have an account?</span> <span onClick = {toggle} className="log-link">Login</span>
+                    </div>
+               </form>
+          </div>
+     )
+}
+
+export default AccountPanel;
