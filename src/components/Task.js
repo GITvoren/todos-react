f2a19200fb6f2fc657d396e638c2b4ba390ac681
GITvoren <tejuco.voren@gmail.com>
2023-05-25 19:16:51 +0800

    added toggle complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,21 @@
 import trash from '../assets/images/trash.png'
 import edit from '../assets/images/edit.png'
 import check from '../assets/images/check.png'
+import {useState} from 'react';
+import complete from '../assets/images/complete.png'
 
 function Task(){
+
+     const [toggleComplete, setToggleComplete] = useState(false)
+
+     
+
      return(
           <div className="task">
-               <p>task descas asdasdasdasdasdas</p>
+               <img className={toggleComplete? "show logo" : "dont-show logo"} src={complete}></img>
+               <p className={toggleComplete? "completed-task" : ""}>task descas asdasdasdasdasdas</p>
                <div className="flex-row">
-                    <img src={check} className="icon"/><button className="task-btn">COMPLETE</button>
+                    <img src={check} onClick={() => setToggleComplete(!toggleComplete)} className="icon"/><button className="task-btn" onClick={() => setToggleComplete(!toggleComplete)}>COMPLETE</button>
                     <img src={edit} className="icon"/><button className="task-btn">EDIT</button>
                     <img src={trash} className="icon"/><button className="task-btn">DELETE</button>
               </div>
